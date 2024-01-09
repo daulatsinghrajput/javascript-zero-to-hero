f29661db536b0a7db807a38dd0659b7423ea4d9a
daulat singh <xtechsingh3103@gmail.com>
2024-01-09 10:11:16 +0530

    add new code

---
+++ operators.js
@@ -0,0 +1,33 @@
+let num1 = 10;
+let num2 = 2;
+
+console.log("arithmetic operater")
+let addition = num1 + num2;
+let subtraction = num1 - num2;
+let product = num1 * num2;
+let reminder = num1 % num2;
+let quationt = num1 / num2;
+
+console.log("addition: ", addition);
+console.log("subtraction: ", subtraction);
+console.log("product: ", product);
+console.log("reminder: ", reminder);
+console.log("quationt: ", quationt);
+
+console.log("comparison operator")
+console.log(num1 == num2);
+console.log(num1 >= num2);
+console.log(num1 <= num2);
+console.log(num1 != num2);
+console.log(num1 > num2);
+console.log(num1 < num2);
+console.log(num1 === num2);
+console.log(num1 !== num2);
+
+console.log("logical operator")
+let a = true;
+let b = false;
+console.log(a || b);
+console.log(a && b);
+console.log(!a);
+console.log(!b);
