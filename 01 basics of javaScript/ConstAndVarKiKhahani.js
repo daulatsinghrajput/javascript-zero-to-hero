1822161e06df74cf59c83f18137457bcf64f55f1
daulat singh <xtechsingh3103@gmail.com>
2024-02-02 12:53:37 +0530

    add new code

---
+++ 01 basics of javaScript/ConstAndVarKiKhahani.js
@@ -0,0 +1,18 @@
+const userId = 232342;
+let userName = "daulat singh";
+let userEmail = "daulat@x.com";
+let userCity = "jaipur";
+let userPassword = "12341234";
+// var userEmail = "daualtz@.com" __-__- it is not use at present time
+// userCity = "jaipur" ____-----_____----- Bad habit 
+
+
+console.log(userId);
+console.log(userName);
+console.log(userEmail);
+console.log(userCity);
+console.log(userPassword);
+
+// simple way --->  
+
+console.table([userId, userName, userEmail, userCity, userPassword])
